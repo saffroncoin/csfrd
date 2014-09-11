73e675d3d3b83e0a849c53b1df353b4d83981c44
Mohammed Roshan <coolron90@gmail.com>
2014-09-12 01:12:29 +0530

    First Push

---
+++ lib/config.py
@@ -0,0 +1,88 @@
+import sys
+import os
+
+"""Variables prefixed with `DEFAULT` should be able to be overridden by
+configuration file and command‐line arguments."""
+
+UNIT = 100000000        # The same across assets.
+
+
+# Versions
+VERSION_MAJOR = 9
+VERSION_MINOR = 42
+VERSION_REVISION = 0
+VERSION_STRING = str(VERSION_MAJOR) + '.' + str(VERSION_MINOR) + '.' + str(VERSION_REVISION)
+
+
+# Counterparty protocol
+TXTYPE_FORMAT = '>I'
+
+TWO_WEEKS = 2 * 7 * 24 * 3600
+MAX_EXPIRATION = 3000 * 60   # Two months
+
+MEMPOOL_BLOCK_HASH = 'mempool'
+MEMPOOL_BLOCK_INDEX = 9999999
+
+
+# SQLite3
+MAX_INT = 2**63 - 1
+
+
+# Bitcoin Core
+OP_RETURN_MAX_SIZE = 80 # bytes
+
+
+# Currency agnosticism
+BTC = 'BTC'
+XCP = 'cSFR'
+
+BTC_NAME = 'Saffroncoin'
+BTC_CLIENT = 'saffroncoind'
+XCP_NAME = 'ConcentratedSFR'
+XCP_CLIENT = 'csfrd'
+
+DEFAULT_RPC_PORT_TESTNET = 49710
+DEFAULT_RPC_PORT = 39710
+
+DEFAULT_BACKEND_RPC_PORT_TESTNET = 29716
+DEFAULT_BACKEND_RPC_PORT = 19710
+
+UNSPENDABLE_TESTNET = 'StrMTo1GjdmYqu66mrhsjb7yed7tvAfrxx'
+UNSPENDABLE_MAINNET = 'SFRXXXXXXXXXXXXXXXXXXXXXXXXXUWLpVr'
+
+ADDRESSVERSION_TESTNET = b'\x3f'
+# PRIVATEKEY_VERSION_TESTNET =
+ADDRESSVERSION_MAINNET = b'\x3f'
+# PRIVATEKEY_VERSION_MAINNET =
+MAGIC_BYTES_TESTNET = b'\x01\xf5\x55\xa4'   # For bip-0010
+MAGIC_BYTES_MAINNET = b'\xcf\x05\x67\xea'   # For bip-0010
+
+BLOCK_FIRST_TESTNET_TESTCOIN = 204215
+BURN_START_TESTNET_TESTCOIN = 204215
+BURN_END_TESTNET_TESTCOIN = 4017708     # Fifty years
+BLOCK_FIRST_TESTNET = 204215
+BURN_START_TESTNET = 204215
+BURN_END_TESTNET = 4017708              # Fifty years
+
+BLOCK_FIRST_MAINNET_TESTCOIN = 204250
+BURN_START_MAINNET_TESTCOIN = 204250
+BURN_END_MAINNET_TESTCOIN = 2500000     # A long time.
+
+BLOCK_FIRST_MAINNET = 204230
+BURN_START_MAINNET = 204250
+BURN_END_MAINNET = 204300
+
+
+# Protocol defaults
+# NOTE: If the DUST_SIZE constants are changed, they MUST also be changed in counterblockd/lib/config.py as well
+    # TODO: This should be updated, given their new configurability.
+# TODO: The dust values should be lowered by 90%, once transactions with smaller outputs start confirming faster: <https://github.com/mastercoin-MSC/spec/issues/192>
+DEFAULT_REGULAR_DUST_SIZE = 5430         # TODO: This is just a guess. I got it down to 5530 satoshis.
+DEFAULT_MULTISIG_DUST_SIZE = 7800        # <https://bitcointalk.org/index.php?topic=528023.msg7469941#msg7469941>
+DEFAULT_OP_RETURN_VALUE = 0
+DEFAULT_FEE_PER_KB = 20000                # Saffroncoin Core default is 20000.  # TODO: Lower 10x later, too.
+
+
+# UI defaults
+DEFAULT_FEE_FRACTION_REQUIRED = .009   # 0.90%
+DEFAULT_FEE_FRACTION_PROVIDED = .01    # 1.00%

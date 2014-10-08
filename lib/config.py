b5393d8cef083ebea87e963fe8888b2fc143162b
Mohammed Roshan <coolron90@gmail.com>
2014-10-08 06:34:11 +0530

    Dust Size

--- lib/config.py
+++ lib/config.py
@@ -77,7 +77,7 @@ BURN_END_MAINNET = 225380
 # NOTE: If the DUST_SIZE constants are changed, they MUST also be changed in csfrd/lib/config.py as well
     # TODO: This should be updated, given their new configurability.
 # TODO: The dust values should be lowered by 90%, once transactions with smaller outputs start confirming faster: <https://github.com/mastercoin-MSC/spec/issues/192>
-DEFAULT_REGULAR_DUST_SIZE = 5430         # TODO: This is just a guess. I got it down to 5530 satoshis.
+DEFAULT_REGULAR_DUST_SIZE = 5430         # TODO: This is just a guess. I got it down to 5430 satoshis.
 DEFAULT_MULTISIG_DUST_SIZE = 7800        # <https://bitcointalk.org/index.php?topic=528023.msg7469941#msg7469941>
 DEFAULT_OP_RETURN_VALUE = 0
 DEFAULT_FEE_PER_KB = 20000                # Saffroncoin Core default is 20000.  # TODO: Lower 10x later, too.
